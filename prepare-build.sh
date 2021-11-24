917c69cb3931b511933dd4b84ddd3e729f03054c
eric1tran <eric1tran@gmail.com>
2021-11-24 16:58:42 +0000

    Updates for bullseye builds

--- prepare-build.sh
+++ prepare-build.sh
@@ -38,19 +38,18 @@ cp -a $FILES $OUT
 cp -a $TOP/debian $OUT
 
 case "$DIST" in
-    jessie)
-        echo "Updating changelog for jessie backport build" >&2
-        dch --changelog $OUT/debian/changelog --local ~bpo8+ --force-distribution --distribution jessie-backports "Automated backport build for jessie"
-        ;;
-
     stretch)
         echo "Updating changelog for stretch backport build" >&2
         dch --changelog $OUT/debian/changelog --local ~bpo9+ --force-distribution --distribution stretch-backports "Automated backport build for stretch"
         ;;
 
     buster)
+        echo "Updating changelog for buster backport build" >&2
+        dch --changelog $OUT/debian/changelog --local ~bpo10+ --force-distribution --distribution buster-backports "Automated backport build for buster"
         ;;
 
+    bullseye)
+        ;;
     *)
         echo "Don't know how to build for a distribution named $DIST" >&2
         exit 1
